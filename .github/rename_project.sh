7a47ecceda2e4d89e23e723c22eb82f6c52ba827
doublethefish <doublethefish@users.noreply.github.com>
2023-11-23 14:53:06 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="lursight"
+original_name="run_test"
+original_urlname="run-test"
+original_description="Awesome run_test created by lursight"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv run_test $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
