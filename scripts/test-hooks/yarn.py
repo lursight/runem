ad013f88ed5f559b299fb29fe30a6df72ad19da7
Frank Harrison <frank@doublethefish.com>
2023-11-29 10:43:54 +0000

    chore(pretty-json): adds jobs to use prettifier via yarn
    ... currently this only targets json files

--- scripts/test-hooks/yarn.py
+++ scripts/test-hooks/yarn.py
@@ -9,3 +9,24 @@ def _job_spellcheck(
 ) -> None:
     es_spellcheck = ["yarn", "run", "spellCheck"]
     run_command(cmd=es_spellcheck, **kwargs)
+
+
+def _job_prettier(
+    **kwargs: typing.Any,
+) -> None:
+    """Runs prettifier on files, including json and maybe yml file.
+
+    TODO: connect me up!
+    """
+    options: Options = kwargs["options"]
+    command_variant = "pretty"
+    if "check_only" in options and options["check_only"]:
+        command_variant = "prettyCheck"
+
+    pretty_cmd = [
+        "yarn",
+        "run",
+        command_variant,
+    ]
+
+    run_command(cmd=pretty_cmd, **kwargs)
