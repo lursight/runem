294f83cd025e971b8898a3c9cd2cfd1ec2d307f0
Frank Harrison <frank@doublethefish.com>
2023-12-04 06:29:25 +0000

    chore(test-and-cov): attempts to add a full config end-to-end test

--- tests/test_runem.py
+++ tests/test_runem.py
@@ -7,7 +7,7 @@ from unittest.mock import Mock, patch
 import pytest
 
 from runem.runem import timed_main
-from runem.types import Config, GlobalSerialisedConfig
+from runem.types import Config, GlobalSerialisedConfig, JobSerialisedConfig
 
 
 def test_runem_basic() -> None:
@@ -54,4 +54,59 @@ def test_runem_basic_with_config(
             "found 1 batches, 1 'mock phase' files, ",
             "filtering for tags set()",
             "skipping phase 'mock phase'",
+        ] == runem_stdout[:3]
+
+
+@patch(
+    "runem.runem.load_config",
+)
+@patch(
+    "runem.runem.find_files",
+)
+def test_runem_with_full_config(
+    find_files_mock: Mock,
+    load_config_mock: Mock,
+) -> None:
+    """End-2-end test with a full config."""
+    global_config: GlobalSerialisedConfig = {
+        "config": {
+            "phases": ("dummy phase 1",),
+            "files": [],
+            "options": [],
+        }
+    }
+    job_config: JobSerialisedConfig = {
+        "job": {
+            "addr": {
+                "file": __file__,
+                "function": "test_runem_with_full_config",
+            },
+            "label": "dummy job label",
+            "when": {
+                "phase": "dummy phase 1",
+                "tags": set(
+                    (
+                        "dummy tag 1",
+                        "dummy tag 2",
+                    )
+                ),
+            },
+        }
+    }
+    full_config: Config = [global_config, job_config]
+    minimal_file_lists = defaultdict(list)
+    minimal_file_lists["mock phase"].append(pathlib.Path("/test") / "dummy" / "path")
+    load_config_mock.return_value = (
+        full_config,
+        pathlib.Path(__file__).parent / ".runem.yml",
+    )
+    find_files_mock.return_value = minimal_file_lists
+    with io.StringIO() as buf, redirect_stdout(buf):
+        # with pytest.raises(SystemExit):
+        timed_main(["--help"])
+        runem_stdout = buf.getvalue().split("\n")
+        assert [
+            "found 1 batches, 1 'mock phase' files, ",
+            "filtering for tags {'dummy tag 1', 'dummy tag 2'}",
+            "will run 1 jobs for phase 'dummy phase 1'",
         ] == runem_stdout[:3]
