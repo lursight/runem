ef54425caaa42e2aebfe067ac32bd1816e3a118b
Frank Harrison <frank@doublethefish.com>
2023-12-09 11:08:28 +0000

    chore(coverage): fixes up coverage, for now

--- runem/command_line.py
+++ runem/command_line.py
@@ -227,7 +227,7 @@ def _initialise_options(
         option["name"]: option["default"] for option in config_metadata.options_config
     }
     if config_metadata.options_config and args.overrides_on:  # pragma: no branch
-        for option_name in args.overrides_on:
+        for option_name in args.overrides_on:  # pragma: no branch
             options[option_name] = True
     if config_metadata.options_config and args.overrides_off:  # pragma: no branch
         for option_name in args.overrides_off:
