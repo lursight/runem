4f2700d37bf6b3c62f4ca8073f81ab0f9d69ab9c
Frank Harrison <frank@doublethefish.com>
2024-11-18 10:57:16 +0000

    chore(clean-types): fixes false-negative from 'isort --check'

--- runem/types/hooks.py
+++ runem/types/hooks.py
@@ -1,10 +1,15 @@
-from enum import Enum
+import enum
 
 
-# before all tasks are run, after config is read
-# BEFORE_ALL = "before-all"
-# after all tasks are done, before reporting
-# AFTER_ALL = "after-all"
-class HookName(Enum):
+class HookName(enum.Enum):
+    """List supported hooks.
+
+    TODO:
+    - before all tasks are run, after config is read
+    - BEFORE_ALL = "before-all"
+    - after all tasks are done, before reporting
+    - AFTER_ALL = "after-all"
+    """
+
     # at exit
     ON_EXIT = "on-exit"
