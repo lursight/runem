99ce85e04431d3ec4101a57c5d678cd2c6fc3022
Frank Harrison <frank@doublethefish.com>
2025-10-05 11:56:18 +0100

    fix(read-write-config): fixes the runem config read needing write permissions
    We do this by removing the '+' in 'r+', becaue the plus sign allows
    write. I probably added it via habit.

--- runem/yaml_utils.py
+++ runem/yaml_utils.py
@@ -14,6 +14,6 @@ def load_yaml_object(yaml_file: pathlib.Path) -> typing.Any:
     """
     # Do a full, untrusted load of the runem config
     # TODO: work out safety concerns of this
-    with yaml_file.open("r+", encoding="utf-8") as file_handle:
+    with yaml_file.open("r", encoding="utf-8") as file_handle:
         full_yaml_object: typing.Any = yaml.full_load(file_handle)
     return full_yaml_object
